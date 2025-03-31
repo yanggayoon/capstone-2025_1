0c9ce2855d9d0cfa86381c0349a331ca42e795f1
yanggayoon <mjc2025504585@gmail.com>
2025-03-31 18:58:38 +0900

    Feat: tabbar 퍼블리싱

--- forest-sause/src/components/menu/TabBar.js
+++ forest-sause/src/components/menu/TabBar.js
@@ -1,17 +1,17 @@
-import '../../styles/menu/TabBar.module.css';
+import styles from '../../styles/menu/TabBar.module.css';
 import { HiOutlineHome } from "react-icons/hi2";
-import { IoCameraOutline } from "react-icons/io5";
+import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
 import { HiOutlinePencilAlt } from "react-icons/hi";
 import { FiUser } from "react-icons/fi";
 
 function TabBar() {
     return(
         <>
-            <div className="menu-box">
-                <HiOutlineHome className="icon" />
-                <IoCameraOutline className="icon" />
-                <HiOutlinePencilAlt className="icon" />
-                <FiUser className="icon" />
+            <div className={styles['menu-box']}>
+                <HiOutlineHome className={styles['icon']} />
+                <IoChatbubbleEllipsesOutline className={styles['icon']} />
+                <HiOutlinePencilAlt className={styles['icon']} />
+                <FiUser className={styles['icon']} />
             </div>
         </>
     )
