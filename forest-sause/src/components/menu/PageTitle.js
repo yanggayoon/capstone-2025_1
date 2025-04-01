df5557600de82ab3fed4b1c1c6fbfc9f788d8b70
yanggayoon <mjc2025504585@gmail.com>
2025-04-01 14:35:29 +0900

    Style: marginBottom x

---
+++ forest-sause/src/components/menu/PageTitle.js
@@ -0,0 +1,16 @@
+function PageTItle( props ) {
+    return (
+        <>
+            <p style={{
+                color: "#5B5B5B",
+                fontSize: "24px",
+                marginBottom: "0",
+                marginLeft: "32px",
+            }}>
+                {props.title}
+            </p>
+        </>
+    )
+}
+
+export default PageTItle;
