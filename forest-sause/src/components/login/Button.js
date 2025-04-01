b2e01ddc021fa5ab12efae85fd07374be89c8ec3
yanggayoon <mjc2025504585@gmail.com>
2025-04-01 10:02:18 +0900

    Feat: 카카오 로그인 > 구글 로그인

--- forest-sause/src/components/login/Button.js
+++ forest-sause/src/components/login/Button.js
@@ -1,12 +1,12 @@
 import styles from '../../styles/login/LoginButton.module.css'
-import kakao from '../../images/kakao.png';
+import google from '../../images/google.png';
 
 function Button() {
     return (
         <>
             <div className={styles['login-Button']}>
-                <img src={kakao} alt="kakao" className={styles['kakao']}/>
-                <p>카카오 로그인</p>
+                <img src={google} alt="google" className={styles['google']}/>
+                <p>구글 로그인</p>
             </div>
 
         </>
