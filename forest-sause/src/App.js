f4254840f26320af8eb1cb58f642790da3a58448
yanggayoon <mjc2025504585@gmail.com>
2025-04-01 14:36:00 +0900

    Feat: 마이페이지 화면 퍼블리싱

--- forest-sause/src/App.js
+++ forest-sause/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import MyPage from './pages/MyPage'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </Router>
   );
