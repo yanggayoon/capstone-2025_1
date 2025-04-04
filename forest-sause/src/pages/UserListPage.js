50e0c12387b1146fa9473b0b495256ebc5c73346
yanggayoon <mjc2025504585@gmail.com>
2025-04-04 12:27:07 +0900

    Feat: 화살표 아이콘 클릭 시 이전 화면으로 이동

--- forest-sause/src/pages/UserListPage.js
+++ forest-sause/src/pages/UserListPage.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FaAngleLeft } from "react-icons/fa6";
 
 function UserListPage() {
+    const navigate = useNavigate();
+
+    const handleBackButtonClick = () => {
+        navigate(-1); 
+    }
+
     return (
         <div style={{
             color: "#5B5B5B",
             padding: "41px 0 44px 0",
         }}>
             <FaAngleLeft style={{
-                fontSize: "24px",
+                fontSize: "20px",
                 marginLeft: "32px",
                 position: "absolute",
-            }} />
+                cursor: "pointer"
+            }}
+            onClick={handleBackButtonClick} />
 
             <p style={{
                 margin: 0,
-                fontSize: "24px",
+                fontSize: "20px",
                 fontWeight: "bold",
                 textAlign: "center",
             }}>사용자 조회</p>
